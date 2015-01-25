6cab2315d85d38abe050eaae6263ee09ea604881
Sergio Ariza <sergioariza@outlook.com>
2015-01-25 18:43:27 +0100

    Add a warning
    Add a warning in login view when incorrect password

--- public/js/login.js
+++ public/js/login.js
@@ -1,22 +1,25 @@
 var app = angular.module("app", ['ngRoute']);
 
-app.controller('UserCtrl', function ($scope, $http, $window) {
-  $scope.user = {username: 'john.doe', password: 'foobar'};
-  $scope.message = '';
+app.controller('UserCtrl', function($scope, $http, $window) {
+    $scope.user = {
+        username: 'john.doe',
+        password: 'foobar'
+    };
 
-  $scope.submit = function () {
-    $http
-      .post('/login', $scope.user)
-      .success(function (data, status, headers, config) {
-        $window.sessionStorage.token = data.token;
-        $window.location = "/home";
-      })
-      .error(function (data, status, headers, config) {
-        // Erase the token if the user fails to log in
-        delete $window.sessionStorage.token;
+    $scope.showWarning = false;
 
-        // Handle login errors here
-        $scope.message = 'Error: Invalid user or password';
-      });
-  };
+    $scope.submit = function() {
+        $http
+            .post('/login', $scope.user)
+            .success(function(data, status, headers, config) {
+                $window.sessionStorage.token = data.token;
+                $window.location = "/home";
+                $scope.showWarning = false;
+            })
+            .error(function(data, status, headers, config) {
+                // Erase the token if the user fails to log in
+                delete $window.sessionStorage.token;
+                $scope.showWarning = true;
+            });
+    };
 });
